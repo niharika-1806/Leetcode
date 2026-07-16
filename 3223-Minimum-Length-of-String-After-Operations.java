b3a5b4e5d57e9d1b9d826342b530fa716b547971
Niharika <niharika2024@lpu.in>
2026-07-16 21:05:35 +0530

    [3223] [Time Beats: 96.33%] [Memory Beats: 51.38%] - LeetPush

---
+++ 3223-Minimum-Length-of-String-After-Operations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumLength(String s) {
+        int freq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                if(freq[i]%2==0){
+                    count+=2;
+                }
+                else{
+                    count+=1;
+                }
+            } 
+        }
+        return count;
+    }
+}
