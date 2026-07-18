b1f4350e4ee51b25dea26aea9a95c008fbc947bf
Niharika <niharika2024@lpu.in>
2026-07-18 11:25:34 +0530

    [1979] [Time Beats: 27.95%] [Memory Beats: 94.14%] - LeetPush

---
+++ DCP-07-26/1979-Find-Greatest-Common-Divisor-of-Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        int max=nums[0];
+        int min=nums[0];
+        for(int n:nums){
+            max=Math.max(n,max);
+            min=Math.min(n,min);
+        }
+
+        while(min!=0){
+            int temp=min;
+            min=max%min;
+            max=temp;
+        }
+        return max;
+    }
+}
