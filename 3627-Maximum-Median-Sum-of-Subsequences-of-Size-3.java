5b140790f1d42f784e7ba6fff08e010103dc3f88
Niharika <niharika2024@lpu.in>
2026-07-20 10:32:59 +0530

    [3627] [Time Beats: 82.95%] [Memory Beats: 76.70%] - LeetPush

---
+++ 3627-Maximum-Median-Sum-of-Subsequences-of-Size-3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long maximumMedianSum(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        long sum=0;
+        for(int i=n-2;i>=n/3;i-=2){
+            sum+=nums[i];
+        }
+        return sum;
+    }
+}
