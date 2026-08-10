4f028585304a86ad6d6b1bc76739c2ff90b0e3b2
Niharika <niharika2024@lpu.in>
2026-08-10 21:25:51 +0530

    [997] [Time Beats: 69.82%] [Memory Beats: 51.37%] - LeetPush

---
+++ 997-Find-the-Town-Judge.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int indegree[]=new int[n+1];
+        int outdegree[]=new int[n+1];
+        for(int []edge:trust){
+            int u=edge[0];
+            int v=edge[1];
+            outdegree[u]++;
+            indegree[v]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(indegree[i]==n-1 && outdegree[i]==0)
+            return i;
+        }
+        return -1;
+
+    }
+}
