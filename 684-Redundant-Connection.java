456d9af3d49e1d61f1004fba409f42d2e629f973
Niharika <niharika2024@lpu.in>
2026-08-10 22:07:20 +0530

    [684] [Time Beats: 91.76%] [Memory Beats: 53.15%] - LeetPush

--- 684-Redundant-Connection.java
+++ 684-Redundant-Connection.java
@@ -1,35 +1,50 @@
 class Solution {
-    public boolean dfs(int u, int v,boolean []visited,List<List<Integer>>adj){
-        if(u==v)
-        return true;
-        visited[u]=true;
-        for(int n: adj.get(u)){
-            if(!visited[n]){
-                if(dfs(n,v,visited,adj))
-                return true;
-            }
+    int parent[];
+    int size[];
+    int find(int n){
+        if(parent[n]==n)
+        return n;
+
+        parent[n]=find(parent[n]);
+        return parent[n];
+    }
+
+    void union(int a,int b){
+        int rootA=find(a);
+        int rootB=find(b);
+
+        // already same components
+        if(rootA==rootB)
+        return;
+
+        if(size[rootA]<size[rootB]){
+            parent[rootB]=rootA;
+            size[rootA]+=size[rootB];
         }
-        return false;
+        else{
+            parent[rootA]=rootB;
+            size[rootB]+=size[rootA];
+        }
     }
     public int[] findRedundantConnection(int[][] edges) {
-        List<List<Integer>>adj=new ArrayList<>();
-        for(int i=0;i<=edges.length;i++){
-            adj.add(new ArrayList<>());
+        int n=edges.length;
+        
+        parent= new int[n+1];
+        size=new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
         }
         for(int edge[]: edges){
             int u=edge[0];
             int v=edge[1];
 
-            boolean []visited=new boolean[edges.length+1];
-
-            // check if a path already exits from u to v, then the current edge is forming the cycle.
-            if(dfs(u,v,visited,adj)){
-                 return edge;
+            // if they have the same parent before union, means another edge is connected them, return the edge that is making a cycle
+            if(find(u)==find(v)){
+                return edge;
             }
-            else{
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
+            union(u,v);
         }
         return new int[0];
     }
