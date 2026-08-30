06ea83bc36774a6de5652237591dc8221a9ac3b4
Niharika <niharika2024@lpu.in>
2026-08-30 11:34:46 +0530

    [4034] [Time Beats: 100.00%] [Memory Beats: -%] - LeetPush

---
+++ 4034-Minimum-Bishop-Moves-to-Reach-Target.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minBishopMoves(int[] source, int[] target) {
+        int m=source.length;
+        int n=target.length;
+
+        if((source[0]+source[1])%2 != (target[0]+target[1])%2)
+            return -1;
+        if(Math.abs(source[0]-target[0])==Math.abs(source[1]-target[1]))
+            return 1;
+        return 2;
+    }
+}
