f57fabe4f0fbc28e77bbb01447ddb525b10228aa
Niharika <niharika2024@lpu.in>
2026-09-02 20:44:24 +0530

    [72] [Time Beats: 65.77%] [Memory Beats: 70.92%] - LeetPush

---
+++ 72-Edit-Distance.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int len1=word1.length();
+        int len2=word2.length();
+
+        int dp[][]=new int[len1+1][len2+1];
+
+        // no characters from word1, so we need to insert all i characters from word2
+        for(int i=0;i<=len2;i++){
+            dp[0][i]=i;
+        }
+        // no characters from word2, so we need to delete all i characters from word1
+        for(int i=0;i<=len1;i++){
+            dp[i][0]=i;
+        }
+
+        for(int i=1;i<=len1;i++){
+            for(int j=1;j<=len2;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+ 
+                    Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
