4a8a88b51ff02b86e91a0fdb33cd7c4ae31ac5ad
Niharika <niharika2024@lpu.in>
2026-09-03 16:57:52 +0530

    [36] [Time Beats: 49.06%] [Memory Beats: 30.14%] - LeetPush

---
+++ 36-Valid-Sudoku.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // creates an array of size 9 that can hold references to hashset objects
+        HashSet<Character>rows[]=new HashSet[9];
+        HashSet<Character>cols[]=new HashSet[9];
+        HashSet<Character>box[]=new HashSet[9];
+
+        for(int i=0;i<9;i++){
+            rows[i]=new HashSet<>();
+            cols[i]=new HashSet<>();
+            box[i]=new HashSet<>();
+        }
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char num=board[i][j];
+                int k=(i/3)*3 + (j/3);
+
+                if(num=='.')
+                continue;
+
+                if(rows[i].contains(num)|| cols[j].contains(num)|| box[k].contains(num))
+                return false;
+
+                rows[i].add(num);
+                cols[j].add(num);
+                box[k].add(num);
+            }
+        }
+        return true;
+    }
+}
