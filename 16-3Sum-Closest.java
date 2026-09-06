6d683484820a4030933d8c8764c6735b62aa378a
Niharika <niharika2024@lpu.in>
2026-09-06 21:23:30 +0530

    [16] [Time Beats: 19.72%] [Memory Beats: 81.10%] - LeetPush

---
+++ 16-3Sum-Closest.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int ans=nums[0]+nums[1]+nums[2];
+
+        for(int i=0;i<n-2;i++){
+            int j=i+1,k=n-1;
+
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(Math.abs(target-sum)< Math.abs(target-ans)){
+                    ans=sum;
+                }
+                if(sum==target)
+                return target;
+
+                else if(sum<target)
+                j++;
+
+                else
+                k--;
+
+            }
+        }
+        return ans;
+    }
+}
